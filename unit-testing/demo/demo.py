53b90b79053ba650d6781473bcba2e06caec3357
Henry <hps1945@live.co.uk>
2022-03-17 09:29:05 +0000

    updated demo code

--- unit-testing/demo/demo.py
+++ unit-testing/demo/demo.py
@@ -1,19 +1,19 @@
-missiles_fired = False
+has_spoken_to_doctor = False
 
 
-def fire_the_missiles() -> None:
-    global missiles_fired
-    missiles_fired = not missiles_fired
+def speak_to_doctor() -> None:
+    global has_spoken_to_doctor
+    has_spoken_to_doctor = False
 
 
 def adder_1(num1: int, num2: int) -> int:
-    return num1 * num2
+    return num1 + num2
 
 
 def adder_2(num1: int, num2: int) -> int:
-    fire_the_missiles()
+    speak_to_doctor()
     return num1 + num2
 
 
 if __name__ == "__main__":
-    print("how would you test these functions?")
+    print(f"{adder_1(3, 7)} == 10")
--- unit-testing/demo/test.py
+++ unit-testing/demo/test.py
@@ -29,6 +29,20 @@ class TestAdder1(unittest.TestCase):
 
 
 # Test Adder 2 below
+class TestAdder2(unittest.TestCase):
+    def test_positive_number(self):
+        # Given
+        num1 = 3
+        num2 = 7
+        expected = 10
+
+        # When
+        actual = demo.adder_2(num1, num2)
+
+        # Then
+        self.assertEqual(expected, actual)
+        self.assertTrue(demo.has_spoken_to_doctor)
+
 
 
 if __name__ == '__main__':
